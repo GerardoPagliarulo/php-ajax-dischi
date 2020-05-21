2363c8108d3b23d72d54957ad498c703856d3aaf
Gerardo Pagliarulo <gerardopagliarulo3@gmail.com>
2020-05-21 13:25:30 +0200

    PHP - Ajax - Dischi

--- index-ajax.php
+++ index-ajax.php
@@ -15,7 +15,7 @@
     <header>
         <div class="container">
             <a href="#">
-                <img src="assets/img/spotify-logo.png" alt="Spotify Logo" class="logo" />
+                <img src="assets/img/spotify-logo.png" alt="Spotify Logo" class="logo">
             </a>
         </div>
     </header>
