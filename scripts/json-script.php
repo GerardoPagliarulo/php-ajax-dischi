bfb8248f7040960417e95adf04c409260c90f1dd
Gerardo Pagliarulo <gerardopagliarulo3@gmail.com>
2020-05-22 17:59:01 +0200

    PHP - Ajax - Dischi

--- scripts/json-script.php
+++ scripts/json-script.php
@@ -1,6 +1,6 @@
 <?php
 // Data
-include_once __DIR__ . '/partials/data/music-info.php';
+include_once __DIR__ . '/../partials/data/music-info.php';
 // JSON
 header('Content-Type: application/json');
 echo json_encode($music_info);
--- src/js/main.js
+++ src/js/main.js
@@ -7,13 +7,15 @@ $(document).ready(function () {
     var template = Handlebars.compile(source);
     // Settings chiamate Ajax
     var settings = {
-        url: "http://localhost:3000/php-ajax-dischi/json-script.php",
+        url: "http://localhost:3000/php-ajax-dischi/scripts/json-script.php",
         method: 'GET'
     };
     // Stampa CD a schermo
     printCd(settings, cdCollection, template);
     // Ricerca per Artista
-    selectArtist(searchArtist, settings, template, cdCollection);
+    searchArtist.keyup(function () {
+        selectArtist(searchArtist, settings, template, cdCollection);
+    });
 });
 /************
  * FUNZIONI *
@@ -42,28 +44,33 @@ function printCd(settings, cdCollection, template) {
 }
 // Funzione: Ricerca per Artista
 function selectArtist(searchArtist, settings, template, cdCollection) {
-    searchArtist.keyup(function () {
-        $('.cd').remove();
-        var nameArtist = searchArtist.val().trim().toLowerCase();
-        // Chiamata Ajax per ricerca artisti
-        $.ajax(settings)
-        .done( function (res) {
-            for (var i = 0; i < res.length; i++) {
-                var cd = res[i];
-                var artist = cd.author;
-                if (nameArtist === '') {
-                    var html = template(cd);
-                    cdCollection.append(html);
-                }
-                else if (artist.toLowerCase() === nameArtist) {
-                //else if (artist.toLowerCase().includes(nameArtist)) {
-                    var html = template(cd);
-                    cdCollection.append(html);
-                }      
+    // Pulizia html
+    cdCollection.html('');
+    var nameArtist = searchArtist.val().trim().toLowerCase();
+    // Chiamata Ajax per ricerca artisti
+    $.ajax(settings)
+    .done( function (res) {
+        for (var i = 0; i < res.length; i++) {
+            var cd = res[i];
+            var content = {
+                poster: cd.poster,
+                author: cd.author,
+                year: cd.year,
+                title: cd.title,
+            };
+            var artist = cd.author;
+            if (nameArtist === '') {
+                var html = template(content);
+                cdCollection.append(html);
             }
-        })
-        .fail(function (error) {
-            console.log('Errore chiamata artisti', error.status);
-        });
-    });    
+            else if (artist.toLowerCase() === nameArtist) {
+            //else if (artist.toLowerCase().includes(nameArtist)) {
+                var html = template(content);
+                cdCollection.append(html);
+            }      
+        }
+    })
+    .fail(function (error) {
+        console.log('Errore chiamata artisti', error.status);
+    });
 }
